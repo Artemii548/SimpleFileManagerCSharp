90bb8edf865280efeaef7bbc03a48857be8bba05
Artemii <artemijnovokresenov548@gmail.com>
2025-03-17 18:07:42 +0500

    попытка запроса пути фйла

--- helloapp/SimpleFileManagerCSharp.cs
+++ helloapp/SimpleFileManagerCSharp.cs
@@ -4,61 +4,64 @@ using System.IO;
 class FileManager
 {
     private string currentDirectory;
-
+ // выбор пути файла
     public FileManager()
     {
-        currentDirectory = Directory.GetCurrentDirectory();
+       Console.WriteLine("Выберите путь..");
+       string? currentDirectory = Console.ReadLine();
     }
+ 
 
   // Просмотр файловой структуры
       public void ListFiles()
     {
-        Console.WriteLine("Contents of " + currentDirectory);
-        foreach (var file in Directory.GetFiles(currentDirectory))
+        Console.WriteLine("Содержание " + currentDirectory);
+         foreach (var file in Directory.GetFiles(currentDirectory))
         {
             Console.WriteLine(Path.GetFileName(file));
         }
-        foreach (var dir in Directory.GetDirectories(currentDirectory))
+         foreach (var dir in Directory.GetDirectories(currentDirectory))
         {
             Console.WriteLine(Path.GetFileName(dir));
         }
     }
- // переход между каталогами 
+   // Переход между каталогами 
     public void ChangeDirectory(string newDirectory)
     {
         string path = Path.Combine(currentDirectory, newDirectory);
-        if (Directory.Exists(path))
+         if (Directory.Exists(path))
         {
             currentDirectory = path;
-            Console.WriteLine("Changed directory to " + currentDirectory);
+            Console.WriteLine("Изменение каталога на " + currentDirectory);
         }
-        else
+         else
         {
-            Console.WriteLine("Directory not found: " + newDirectory);
+            Console.WriteLine("Каталог не найден: " + newDirectory);
         }
     }
     // Копирование файлов и каталогов
 
     static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
-{
+   {
    
-    var dir = new DirectoryInfo(sourceDir);
-
-    if (!dir.Exists) throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}")
+      var dir = new DirectoryInfo(sourceDir);
+ 
+       if (!dir.Exists) throw new DirectoryNotFoundException($"Исходный каталог не найден: {dir.FullName}");
     
-    DirectoryInfo dirs = dir.GetDirectories();
+     // DirectoryInfo[] dirs = dir.GetDirectories();
   
-    Directory.CreateDirectory(destinationDir);
+      Directory.CreateDirectory(destinationDir);
    
-    foreach (FileInfo file in dir.GetFiles())
-    {
-        string targetFilePath = Path.Combine(destinationDir, file.Name);
-        file.CopyTo(targetFilePath);
-    }
+       foreach (FileInfo file in dir.GetFiles())
+       {
+          string targetFilePath = Path.Combine(destinationDir, file.Name);
+          file.CopyTo(targetFilePath);
+        }
     
+    }
+
 }
-}
-// удаление файлов
+
 
 
 
